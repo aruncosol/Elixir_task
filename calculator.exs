4537480bf4612bc91738ce3e217d54f1ccdcdf88
aruncosol <arun@cosol.in>
2023-04-03 16:43:11 +0530

    patter_matching_method

--- calculator.exs
+++ calculator.exs
@@ -4,62 +4,80 @@ defmodule Calculator do
   end
 
   defp get_input do
-    IO.gets("Enter the 1'st number\n")
-    |> String.trim_trailing()
-    |> String.to_integer()
-    |> get_second_input()
+    {num1, num2} = get_numbers()
+    operation = get_operation()
+
+    perform_operation(num1, num2, operation)
   end
 
-  defp get_second_input(num1) do
-    IO.gets("Enter the 2'nd number\n")
-    |> String.trim_trailing()
-    |> String.to_integer()
-    |> perform_operation(num1)
+  defp get_numbers do
+    IO.puts("Enter the 1'st number")
+    num1 = IO.gets() |> String.trim() |> String.to_integer()
+
+    IO.puts("Enter the 2'nd number")
+    num2 = IO.gets() |> String.trim() |> String.to_integer()
+
+    {num1, num2}
   end
 
-  defp perform_operation(num1, num2) do
-    IO.gets("Enter Corresponding number\n(1)Add\n(2)Sub\n(3)Mul\n(4)Div\n")
-    |> String.trim_trailing()
-    |> String.to_integer()
-    |> get_answer(num1, num2)
+  defp get_operation do
+    IO.puts("Enter Corresponding number\n(1)Add\n(2)Sub\n(3)Mul\n(4)Div")
+    IO.gets() |> String.trim() |> String.to_integer()
   end
 
-  defp get_answer(num1, num2, 1), do: add(num1, num2)
-  defp get_answer(num1, num2, 2), do: sub(num1, num2)
-  defp get_answer(num1, num2, 3), do: mul(num1, num2)
-  defp get_answer(num1, num2, 4), do: division(num1, num2)
-  defp get_answer(_, _, _), do: IO.puts("Please enter valid input")
-  get_input()
+  defp perform_operation(num1, num2, 1) do
+    add(num1, num2)
+    acknowledgement()
+  end
+
+  defp perform_operation(num1, num2, 2) do
+    sub(num1, num2)
+    acknowledgement()
+  end
+
+  defp perform_operation(num1, num2, 3) do
+    mul(num1, num2)
+    acknowledgement()
+  end
+
+  defp perform_operation(num1, num2, 4) do
+    division(num1, num2)
+    acknowledgement()
+  end
+
+  defp perform_operation(_, _, _) do
+    IO.puts("Please enter a valid input.")
+    get_input()
+  end
+
+  defp acknowledgement do
+    IO.puts("Are you done? or would you like to do it again (yes/y) or (no/n)")
+    confirm = IO.gets() |> String.trim()
+
+    case confirm do
+      "n" -> IO.puts("Good bye, Thank you for using this session")
+      "y" -> IO.puts("Welcome again...") |> get_input()
+      _ -> acknowledgement()
+    end
+  end
 
   defp add(num1, num2) do
     IO.puts("Ans : #{num1 + num2}")
-    acknowledgement()
   end
 
   defp sub(num1, num2) do
-    IO.puts("Ans : #{num1 - num2}")
-    acknowledgement()
+    if num2 > num1 do
+      sub(num2, num1)
+    else
+      IO.puts("Ans : #{num1 - num2}")
+    end
   end
 
   defp mul(num1, num2) do
     IO.puts("Ans : #{num1 * num2}")
-    acknowledgement()
   end
 
   defp division(num1, num2) do
     IO.puts("Ans : #{div(num1, num2)}")
-    acknowledgement()
   end
-
-  defp acknowledgement do
-    IO.gets("Are you done? or would you like to do it again (yes/y) or (no/n)")
-    |> String.trim_trailing()
-    |> check_answer()
-  end
-
-  defp check_answer("n"), do: IO.puts("Good bye, Thank you for using this session")
-  defp check_answer("y"), do: IO.puts("Welcome again...")
-  get_input()
-  defp check_answer(_), do: IO.puts("Invalid input please enter valid input Yes(/y) or No(/n)")
-  acknowledgement()
 end
